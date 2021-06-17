15b3db094db6c96aa4a4d0353783ec8adabda8ec
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-06-17 01:27:49 -0700

    dashboard func

--- contacts/views.py
+++ contacts/views.py
@@ -17,6 +17,7 @@ def contact(request):
     #  Check if user has made inquiry already
     if request.user.is_authenticated:
       user_id = request.user.id
+    # Check for current listing and current user
       has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
       if has_contacted:
         messages.error(request, 'You have already made an inquiry for this listing')
